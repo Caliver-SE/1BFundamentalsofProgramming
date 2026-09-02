2c1af87cf574c576cff186472d700f11bd27d6ea
Caliver <caliver91305@gmail.com>
2026-09-03 00:37:54 +0800

    upload

---
+++ MyProject/src/Assignment3.java
@@ -0,0 +1,19 @@
+public class Assignment3 {
+
+    public static void main(String[] args) {
+
+        String firstname = "Caliver";
+        String lastname = "Partible";
+        String address = "Pozorrubio";
+        String mobilenumber = "09954027645";
+        int age = 20;
+        int gwa = 97;
+
+        System.out.println("Firstname: " + firstname);
+        System.out.println("Lastname: " + lastname);
+        System.out.println("Adress: " + address);
+        System.out.println("Age: "  + age);
+        System.out.println("GWA: " + gwa);
+
+    }
+}
